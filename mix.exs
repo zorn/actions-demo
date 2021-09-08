99f842b2a7d457b9f60302c2f7f68dfaeab98fd9
Mike Zornek <zorn@zornlabs.com>
2021-09-08 12:00:04 -0400

    lower valid elixir version for project

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Hello.MixProject do
     [
       app: :hello,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
